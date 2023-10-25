a1c3c168e484da6519040062b10cf2baa7444d0a
s3xmaxman <badside3919@gmail.com>
2023-10-26 08:19:14 +0900

    implement all the features

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -6,8 +6,10 @@ import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
-  
-  const userInfo = {}
+  if (!user) return null; // to avoid typescript warnings
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
     id: user.id,
@@ -22,7 +24,7 @@ async function Page() {
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
       <p className='mt-3 text-base-regular text-light-2'>
-        Complete your profile now, to use Threds.
+        Complete your profile now, to use Threads.
       </p>
 
       <section className='mt-9 bg-dark-2 p-10'>
